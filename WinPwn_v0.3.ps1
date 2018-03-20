02e273347dc250b90591973f3bd0eb63f5c74efa
SecureThisShit <noreply@github.com>
2018-03-20 19:24:10 +0100

    Update WinPwn_v0.3.ps1

--- WinPwn_v0.3.ps1
+++ WinPwn_v0.3.ps1
@@ -559,7 +559,7 @@ function proxydetect
              #Proxy
             Write-Host -ForegroundColor Yellow 'Setting up Powershell-Session Proxy Credentials...'
             $Wcl = new-object System.Net.WebClient
-            $Wcl.Headers.Add('user-agent', 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0')
+            #$Wcl.Headers.Add('user-agent', 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0')
             $Wcl.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
         }
         else
